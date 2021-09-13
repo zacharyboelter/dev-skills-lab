43c1de08e79a1ab8a8e7d3d83b800b53102cb083
Zachary Boelter <zboelter@gmail.com>
2021-09-13 07:16:56 -0500

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Get user input and set it to an li that has a small x button for removal
 //Take li and append it to ul (list)
 //Make sure input field is blank
+//Refer to zillow script for functions help
 
 //when add-button is clicked, run function
 $(".add-btn").on("click", function (evt) {
